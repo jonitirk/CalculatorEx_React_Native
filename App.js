52590298e5831475e4d915b09186aef1a6934e06
jonitirk <jonitirkkonenmusic@gmail.com>
2024-01-23 22:35:05 +0200

    Vienti GitHubiin

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [result, setResult] = useState('');
+  const [first, setFirst] = useState('');
+  const [second, setSecond] = useState('');
+
+  const sumValues = () => {
+    setResult(parseInt(first) + parseInt(second));
+  };
+
+  const decreaseValues = () => {
+    setResult(parseInt(first) - parseInt(second));
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput keyboardType="numeric" style={{ width: 100, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={first => setFirst(first)} value={first} />
+      <TextInput keyboardType="numeric" style={{ width: 100, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={second => setSecond(second)} value={second} />
+
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={sumValues} title="+" />
+        <Button onPress={decreaseValues} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
